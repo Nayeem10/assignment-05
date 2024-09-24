23f17f0616129912a6e1594afbc2062d47c7b2e9
Nayeem10 <nayeemhossain2110@gmail.com>
2024-09-24 22:46:35 +0600

    input resets now

--- scripts/donation.js
+++ scripts/donation.js
@@ -31,3 +31,60 @@ document.getElementById('button-container').addEventListener('click', function (
 
     clickedButton === childs[0] ? toggle('history', 'donations') : toggle('donations', 'history');
 })
+
+document.getElementById('donate-noakhali').addEventListener('click', calculateDonation);
+document.getElementById('donate-feni').addEventListener('click', calculateDonation);
+document.getElementById('donate-quota').addEventListener('click', calculateDonation);
+
+function convert(donatedAmount) {
+    for(const alpha of donatedAmount){
+        if(alpha < '0' || alpha > '9') return 0; 
+    }
+    const amount = parseFloat(donatedAmount);
+    if (isNaN(amount) || amount <= 0) return 0;
+    return amount;
+}
+
+function calculateDonation(event) {
+    const clickedButton = event.target;
+    const parentSection = clickedButton.parentElement;
+    const previousDonations = parentSection.children[0].children[1].innerText;
+    const donatedAmount = parentSection.children[3].value;
+    const amount = convert(donatedAmount);
+
+    const currentBalance = document.getElementById('current-balance');
+    const balance = convert(currentBalance.innerText);
+    parentSection.children[3].value = '';
+    if (!amount || balance < amount) {
+        alert('Invalid Donation Amount');
+        return;
+    }
+    parentSection.children[0].children[1].innerText = parseFloat(previousDonations) + amount;
+    currentBalance.innerText = balance - amount;
+
+    let historyDetails = amount + ' Taka is Donated for ';
+
+    if (clickedButton.id === 'donate-feni') {
+        historyDetails += 'Flood Relief in Feni, Bangladesh';
+    }else if (clickedButton.id === 'donate-noakhali') {
+        historyDetails += 'Flood Relief in Noakhali, Bangladesh';
+    }else if(clickedButton.id === 'donate-quota'){
+        historyDetails += 'Aid for Injured in the Quota Movement, Bangladesh'
+    }
+
+    const newHistory = document.createElement('div');
+    const header = document.createElement('h2');
+    header.innerText = historyDetails;
+    const para = document.createElement('p');
+    para.innerText = 'Date: ' + new Date();
+
+    header.classList.add('text-xl', 'font-bold', 'my-5');
+    para.classList.add('font-[300]', 'text-dark-0', 'mb-5');
+    newHistory.appendChild(header);
+    newHistory.appendChild(para);
+    newHistory.classList.add('border', 'p-6', 'rounded-xl', 'shadow-xl')
+    document.getElementById('history').appendChild(newHistory);
+
+    const modal = document.getElementById('my-modal');
+    modal.showModal();
+}
