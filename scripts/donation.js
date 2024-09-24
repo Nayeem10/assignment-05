2b422fd0a689599f8ce8d91ea954ebf07f7bff24
Nayeem10 <nayeemhossain2110@gmail.com>
2024-09-24 22:26:10 +0600

    button toggler added

--- scripts/donation.js
+++ scripts/donation.js
@@ -12,3 +12,22 @@ tailwind.config = {
         }
     }
 }
+
+function toggle(sectionID1, sectionID2) {
+    document.getElementById(sectionID1).classList.add('hidden');
+    document.getElementById(sectionID2).classList.remove('hidden');
+}
+
+document.getElementById('button-container').addEventListener('click', function (event) {
+    const clickedButton = event.target;
+    if (clickedButton.tagName !== 'BUTTON') {
+        return;
+    }
+    const childs = document.getElementById('button-container').children;
+    for (const child of childs) {
+        child.classList.remove('bg-parrot', 'hover:bg-[#92e12c]');
+    }
+    clickedButton.classList.add('bg-parrot', 'hover:bg-[#92e12c]');
+
+    clickedButton === childs[0] ? toggle('history', 'donations') : toggle('donations', 'history');
+})
